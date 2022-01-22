d05677568fec86433edf1a5dc059ebcba6dd41aa
XhmikosR <xhmikosr@gmail.com>
2022-01-22 15:56:13 +0200

    Update all the things
    * add .editorconfig
    * enforce LF
    * add ESLint
    * switch to GitHub Actions
    * remove debug dependency
    * add LICENSE

--- test/test.js
+++ test/test.js
@@ -1,53 +1,53 @@
+/* eslint-env mocha */
+
 'use strict';
 
-const detective = require('../');
 const assert = require('assert');
+const detective = require('../index.js');
 
-describe('detective-sass', function() {
-  function test(src, deps, opts) {
-    assert.deepEqual(detective(src, opts), deps);
-  }
+function test(source, dependencies, options) {
+  assert.deepEqual(detective(source, options), dependencies);
+}
 
-  describe('throws', function() {
-    it('does not throw for empty files', function() {
-      assert.doesNotThrow(function() {
+describe('detective-sass', () => {
+  describe('error handling', () => {
+    it('does not throw for empty files', () => {
+      assert.doesNotThrow(() => {
         detective('');
       });
     });
 
-    it('throws if the given content is not a string', function() {
-      assert.throws(function() {
-        detective(function() {});
-      });
+    it('throws if the given content is not a string', () => {
+      assert.throws(() => {
+        detective(() => {});
+      }, Error, 'content is not a string');
     });
 
-    it('throws if called with no arguments', function() {
-      assert.throws(function() {
+    it('throws if called with no arguments', () => {
+      assert.throws(() => {
         detective();
-      });
+      }, Error, 'src not given');
     });
 
-    it('does not throw on broken syntax', function() {
-      assert.doesNotThrow(function() {
+    it('does not throw on broken syntax', () => {
+      assert.doesNotThrow(() => {
         detective('@');
       });
     });
-  });
 
-  it('dangles the parsed AST', function() {
-    detective('@import "_foo.sass";');
-    assert.ok(detective.ast);
-  });
-
-  describe('when there is a parse error', function() {
-    it('supplies an empty object as the "parsed" ast', function() {
+    it('supplies an empty object as the "parsed" ast', () => {
       detective('|');
       assert.deepEqual(detective.ast, {});
     });
   });
 
-  describe('sass', function() {
-    it('returns the dependencies of the given .sass file content', function() {
+  describe('sass', () => {
+    it('dangles the parsed AST', () => {
+      detective('@import "_foo.sass";');
+      assert.ok(detective.ast);
+    });
+
+    it('returns the dependencies of the given .sass file content', () => {
       test('@import _foo', ['_foo']);
       test('@import        _foo', ['_foo']);
       test('@import reset', ['reset']);
