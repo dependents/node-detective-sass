96526c3202c2136323fc8420afc8e910de4154ec
XhmikosR <xhmikosr@gmail.com>
2022-02-22 12:42:10 +0200

    Update all the things (#15)
    * add .editorconfig
    * enforce LF
    * add ESLint
    * switch to GitHub Actions
    * remove debug dependency
    * add LICENSE

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
+const { debuglog } = require('util');
 const Walker = require('node-source-walk');
 const sass = require('gonzales-pe');
-const debug = require('debug')('detective-sass');
+
+const debug = debuglog('detective-sass');
 
 /**
  * Extract the @import statements from a given sass file's content
@@ -11,26 +13,25 @@ const debug = require('debug')('detective-sass');
  * @return {String[]}
  */
 module.exports = function detective(fileContent) {
-  if (typeof fileContent === 'undefined') { throw new Error('content not given'); }
-  if (typeof fileContent !== 'string') { throw new Error('content is not a string'); }
+  if (typeof fileContent === 'undefined') throw new Error('content not given');
+  if (typeof fileContent !== 'string') throw new Error('content is not a string');
 
   let dependencies = [];
-  let ast;
+  let ast = {};
 
   try {
-    debug('content: ' + fileContent);
+    debug('content: %s', fileContent);
     ast = sass.parse(fileContent, { syntax: 'sass' });
-  } catch (e) {
-    debug('parse error: ', e.message);
-    ast = {};
+  } catch (error) {
+    debug('parse error: %s', error.message);
   }
 
   detective.ast = ast;
 
   const walker = new Walker();
 
-  walker.walk(ast, function(node) {
-    if (!isImportStatement(node)) { return; }
+  walker.walk(ast, (node) => {
+    if (!isImportStatement(node)) return;
 
     dependencies = dependencies.concat(extractDependencies(node));
   });
@@ -39,26 +40,22 @@ module.exports = function detective(fileContent) {
 };
 
 function isImportStatement(node) {
-  if (!node || node.type !== 'atrule') { return false; }
-  if (!node.content.length || node.content[0].type !== 'atkeyword') { return false; }
+  if (!node || node.type !== 'atrule') return false;
+  if (node.content.length === 0 || node.content[0].type !== 'atkeyword') return false;
 
-  var atKeyword = node.content[0];
+  const atKeyword = node.content[0];
 
-  if (!atKeyword.content.length) { return false; }
+  if (atKeyword.content.length === 0) return false;
 
-  var importKeyword = atKeyword.content[0];
+  const importKeyword = atKeyword.content[0];
 
-  if (importKeyword.type !== 'ident' || importKeyword.content !== 'import') { return false; }
+  if (importKeyword.type !== 'ident' || importKeyword.content !== 'import') return false;
 
   return true;
 }
 
 function extractDependencies(importStatementNode) {
   return importStatementNode.content
-  .filter(function(innerNode) {
-    return innerNode.type === 'string' || innerNode.type === 'ident';
-  })
-  .map(function(identifierNode) {
-    return identifierNode.content.replace(/["']/g, '');
-  });
+    .filter((innerNode) => innerNode.type === 'string' || innerNode.type === 'ident')
+    .map((identifierNode) => identifierNode.content.replace(/["']/g, ''));
 }
